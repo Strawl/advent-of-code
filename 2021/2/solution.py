07ad700e519219fabb8b4e099253415e1055d14b
gya18170 <gya18170@spengergasse.at>
2021-12-02 16:58:48 +0100

    Adding day2

---
+++ 2021/2/solution.py
@@ -0,0 +1,30 @@
+with open("input.txt","r") as f:
+    data = [x.strip().split(" ") for x in f.readlines()]
+    horizontal = 0
+    depth=0
+    for x in data: 
+        match x[0]:
+            case "forward":
+                horizontal += int(x[1])
+            case "up":
+                depth -= int(x[1]) 
+            case "down":
+                depth += int(x[1])
+    print(horizontal * depth)
+    aim = 0
+    horizontal = 0
+    depth=0
+    for x in data: 
+        match x[0]:
+            case "forward":
+                horizontal += int(x[1])
+                depth += aim * int(x[1])
+            case "up":
+                aim -= int(x[1]) 
+            case "down":
+                aim += int(x[1])
+    print(horizontal * depth)
+
+
+
+                
